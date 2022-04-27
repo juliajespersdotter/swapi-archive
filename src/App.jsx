c7c5fc63a324100a636f3a7d04a0ca7338961754
Julia <79936455+juliajespersdotter@users.noreply.github.com>
2022-04-27 08:56:53 +0200

    added not found page.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import FilmsPage from './pages/FilmsPage'
 import HomePage from './pages/HomePage'
 import FilmInfoPage from './pages/FilmInfoPage'
 import PeopleInfoPage from './pages/PeopleInfoPage'
+import NotFound from './pages/NotFound'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
@@ -21,6 +22,7 @@ function App() {
 					<Route path="/films" element={<FilmsPage />} />
 					<Route path="/films/:id" element={<FilmInfoPage />} />
 					<Route path="/people/:id" element={<PeopleInfoPage />} />
+					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</Container>
       
